11b8a31a85c5c52f5eab79039ffda776622a5bf9
Valeriy Anysenko <168164564+vlrnsnk@users.noreply.github.com>
2025-01-03 23:16:40 -0500

    Add dummy get and post endpoints

--- app.js
+++ app.js
@@ -9,6 +9,14 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 8000;
 
+app.get('/', (req, res, next) => {
+  res.send('Hello World!');
+});
+
+app.post('/api/v1/envelopes', (req, res, next) => {
+  res.status(201).send('POST');
+});
+
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
